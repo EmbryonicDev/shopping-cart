c56cf7b0fed3d58699a8071355356729d7cb30bf
djdevlam-apple <embryonic.dev@gmail.com>
2022-10-14 15:48:15 +0200

    Change header h1 -> Tycoon's Outlet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 export default function Header() {
   return (
     <div className="header">
-      <h1>Shop Demo</h1>
+      <h1>Tycoon's Outlet</h1>
       <div className="header-links">
         <a href="/">Home</a>
         <a href="/shop">Shop</a>
