7dc4b3bb02da96fcae8835e6a387584ea71d854c
EmbryonicDev <embryonic.dev@gmail.com>
2022-10-15 19:52:32 +0200

    RouterSwitch: set state for shop items & use as prop for Shop

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,13 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Shop from './components/Shop/Shop';
 import Cart from './components/Cart/Cart';
+import { useState } from 'react';
+import shopItems from './shopItems';
 
 const RouteSwitch = () => {
+  const [shopItemsArr] = useState(shopItems);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop" element={
+          <Shop
+            items={shopItemsArr}
+          />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
