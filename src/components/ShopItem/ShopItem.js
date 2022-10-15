a7945b3c736c5ad9cdf62f219f168667f9945a37
EmbryonicDev <embryonic.dev@gmail.com>
2022-10-15 18:26:24 +0200

    Add properties to ShopItems

--- src/components/ShopItem/ShopItem.js
+++ src/components/ShopItem/ShopItem.js
@@ -7,11 +7,11 @@ const ShopItem = (props) => {
     <div className="shopItem">
       <img src={image} alt={name} />
       <h2 className="itemName">{name}</h2>
-      <h3 className="price">$ {price}</h3>
-      <form action="#">
-        <label htmlFor="num">Add to Cart: </label>
-        <input type="number" />
-      </form>
+      <h2 className="price">Price: ${price}</h2>
+      <div className='cartQtyWrap'>
+        <input type="number" min='0' max='5' placeholder='0' />
+        <button>Add</button>
+      </div>
     </div>
   )
 }
