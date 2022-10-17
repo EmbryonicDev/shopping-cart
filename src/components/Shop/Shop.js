91df94263bc06b55790efbd1ba916fea95a41eb6
djdevlam-apple <embryonic.dev@gmail.com>
2022-10-17 17:44:23 +0200

    Modify Shop to use price instead of displayPrice

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,7 +9,7 @@ export default function Shop(props) {
       <ShopItem
         name={item.name}
         image={item.image}
-        price={item.displayPrice}
+        price={item.price}
         key={index}
         itemToCart={() => itemToCart(item.name)}
       />
