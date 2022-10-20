14e9a10d639d87c3467a7dde3c8f473bed10d7a3
djdevlam-apple <embryonic.dev@gmail.com>
2022-10-20 17:58:47 +0200

    Add div with h1 text to EmptyCart: Your cart is empty

---
+++ src/components/EmptyCart/EmptyCart.js
@@ -0,0 +1,11 @@
+import './EmptyCart.css'
+
+const EmptyCart = () => {
+  return (
+    <div className="emptyCartWrap">
+      <h1>Your Cart is Empty</h1>
+    </div>
+  )
+}
+
+export default EmptyCart;
