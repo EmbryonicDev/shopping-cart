e2b86b68a1f66886afdaf7e98738868c39ea6230
djdevlam-apple <embryonic.dev@gmail.com>
2022-10-20 15:10:40 +0200

    Add conditional to display Millions / Billions after price

--- src/components/Cart/CartRow.js
+++ src/components/Cart/CartRow.js
@@ -1,11 +1,18 @@
 const CartRow = (props) => {
   const { name, quantity, price, totalPrice } = props
+
   return (
     <tr>
       <td>{name}</td>
       <td>{quantity}</td>
-      <td>${price / 1000000} Million</td>
-      <td>${totalPrice / 1000000} Million</td>
+      <td>${price} Million</td>
+      <td>
+        {
+          totalPrice < 1000 ?
+            `$ ${totalPrice} Million` :
+            `$ ${totalPrice / 1000} Billion`
+        }
+      </td>
     </tr>
   )
 }
